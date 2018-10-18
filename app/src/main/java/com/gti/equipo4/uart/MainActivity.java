d9d37a4bc254e547464ac439bc57f79f8c16646c
Joan Ciprià <joancipria@gmail.com>
2018-10-18 13:37:17 +0200

    Initial commit

---
+++ app/src/main/java/com/gti/equipo4/uart/MainActivity.java
@@ -0,0 +1,32 @@
+package com.gti.equipo4.uart;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Lista de UART disponibles: " + ArduinoUart.disponibles());
+        ArduinoUart uart = new ArduinoUart("UART0", 115200);
+        Log.d(TAG, "Mandado a Arduino: H");
+        uart.escribir("H");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Log.w(TAG, "Error en sleep()", e);
+        }
+        String s = uart.leer();
+        Log.d(TAG, "Recibido de Arduino: "+s);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
